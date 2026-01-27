b2acf7362b98869eb2036a3b520e37525916a2ed
xsalles <xs.salles@gmail.com>
2026-01-27 10:22:44 -0300

    feat: created the real implementation of BcryptHasher

---
+++ src/modules/users/infrastructure/cryptography/bcrypt-hasher.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { Hasher } from '../../domain/entities/hasher';
+import { compare, hash } from 'bcryptjs';
+
+@Injectable()
+export class BcryptHasher implements Hasher {
+  hash(plainPassword: string): Promise<string> {
+    return hash(plainPassword, 10);
+  }
+
+  compare(plainPassword: string, hash: string): Promise<boolean> {
+    return compare(plainPassword, hash);
+  }
+}
