7c1ffb446113b18c63f7cae030cec222a454d886
xsalles <xs.salles@gmail.com>
2026-01-27 09:44:26 -0300

    refactored: changed update user to just save

--- src/modules/users/domain/repositories/user-repository.ts
+++ src/modules/users/domain/repositories/user-repository.ts
@@ -4,5 +4,5 @@ export abstract class UserRepository {
     abstract createUser(user: User) : Promise<void>;
     abstract findByEmail(email: string) : Promise<User | null>;
     abstract findById(id: string) : Promise<User | null>;
-    abstract updateUser(user: User) : Promise<void>;
+    abstract save(user: User) : Promise<void>;
 }
