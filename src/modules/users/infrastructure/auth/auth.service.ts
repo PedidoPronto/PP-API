0e49d27c8b720de107fafe07676ab6b9c8247a9d
xsalles <xs.salles@gmail.com>
2026-01-28 20:30:10 -0300

    feat: now the jwt security token is being generated

--- src/modules/users/infrastructure/auth/auth.service.ts
+++ src/modules/users/infrastructure/auth/auth.service.ts
@@ -10,20 +10,39 @@ export class AuthService {
   async generateBasicTokens(
     user: UserPayloadDto,
   ): Promise<{ accessToken: string; refreshToken: string }> {
-    const payload = { sub: user.id, email: user.email, role: user.role };
+    const payloadAccess = { sub: user.id, email: user.email, role: user.role };
 
     const [accessToken, refreshToken] = await Promise.all([
-      this.jwtService.signAsync(payload, {
+      this.jwtService.signAsync(payloadAccess, {
         secret: jwtSecrets.access,
         expiresIn: '15m',
       }),
-      this.jwtService.signAsync(payload, {
-        secret: jwtSecrets.refresh,
-        expiresIn: '7d',
-      }),
+      this.jwtService.signAsync(
+        { sub: user.id },
+        {
+          secret: jwtSecrets.refresh,
+          expiresIn: '7d',
+        },
+      ),
     ]);
 
     return { accessToken, refreshToken };
   }
 
+  async generateSecurityToken(user: UserPayloadDto): Promise<string> {
+    const payload = {
+      sub: user.id,
+      email: user.email,
+      role: user.role,
+      scope: 'high_security',
+      verified2fa: true,
+    };
+
+    const securityToken = await this.jwtService.signAsync(payload, {
+      secret: jwtSecrets.security,
+      expiresIn: '30m',
+    });
+
+    return securityToken;
+  }
 }
